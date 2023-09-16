8044c2759feba42157bbdd9054b0ae607d01dd75
Allena101 <96913339+Allena101@users.noreply.github.com>
2023-09-16 18:46:30 +0200

    Added if even loop

--- oj.py
+++ oj.py
@@ -3,6 +3,13 @@ for i in range(10):
     x += 1
     print(x)
 
-name_list = ['Magnus', 'Jonas', 'Erik']
+name_list = ["Magnus", "Jonas", "Erik"]
 for i in name_list:
     print(i)
+
+# Second loop
+for i in range(20):
+    if i % 2 == 0:
+        print(f"{i} is even.")
+    else:
+        print(f"{i} is odd.")
